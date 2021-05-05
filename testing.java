e783e01fe2349c9583a230865ede1d419fca5875
Thierry Musigazi <37936539+gitdetiti@users.noreply.github.com>
2021-05-05 16:15:00 +0200

    Update testing.java

--- testing.java
+++ testing.java
@@ -1,5 +1,5 @@
-Testing the integration of Jenkins with GitHub
-
-Blablabla
-
-Yadiyadadada
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
